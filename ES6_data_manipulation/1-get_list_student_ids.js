a2ff65ebcda7cb903bf0e70f3a87d25e797e4a2a
John Nkp <john.nkp1@gmail.com>
2025-04-08 16:11:51 +1000

    1-main.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
-// function getListStudentIds that returns an array of ids from a list of objects
+// function getListStudentIds returns an array of ids from a list of objects
 export default function getListStudentIds(students) {
   if (Array.isArray(students)) {
     return students.map((items) => items.id);
