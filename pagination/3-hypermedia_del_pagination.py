0955afb2429ed61304e1b3191727557b8a4734fd
John Nkp <john.nkp1@gmail.com>
2025-05-20 22:21:12 +1000

    pagination/

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -38,7 +38,7 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """ return a dictionary """
-        assert isinstance(index, int) and type(page_size) == int
+        assert isinstance(index, int) and isinstance(page_size, int)
         assert 0 <= index < len(self.indexed_dataset())
         pages = []
         next_index = index + page_size
